ff2ec0d539561b737b9ca4a32db9e6e18e7fdbbf
Maria Saleri <mariasaleri@gmail.com>
2019-05-29 15:37:58 +0200

    Popolazione

--- zoo_schema.sql
+++ zoo_schema.sql
@@ -10,20 +10,19 @@ CREATE TABLE AREA_TEMATICA (
 
 CREATE TABLE ALLOGGIO (
 	NUMERO INT NOT NULL,
-	NOME  VARCHAR(20),
-	TIPO  VARCHAR(20),
+	NOME  VARCHAR(40),
+	TIPO  VARCHAR(40),
 	NOME_AREA  VARCHAR(20)  NOT NULL,
 	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (NUMERO),
+	PRIMARY KEY (NUMERO, NOME_AREA),
 	FOREIGN KEY (NOME_AREA) REFERENCES AREA_TEMATICA(NOME)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ANIMALE (
 	ID VARCHAR(10) NOT NULL, 
 	NOME VARCHAR(20),
-	SPECIE VARCHAR(20) NOT NULL,
-	HABITAT VARCHAR(20),
-	BIOMA VARCHAR(20),
+	SPECIE VARCHAR(30) NOT NULL,
+	HABITAT enum('Calotta polare','Tundra','Taiga','Foresta decidua','Steppa e prateria','Foresta pluviale temperata','Foresta pluviale tropicale','Macchia mediterranea','Giungla','Deserto sabbioso','Deserto roccioso','Deserto semiarido','Steppa arida','Savana erbosa','Savana alberata','Foresta subtropicale secca','Tundra alpina','Vegetazione alpina') DEFAULT NULL,
 	PESO INT UNSIGNED,
 	ETA INT UNSIGNED, 	
 	SESSO ENUM('M', 'F'),
@@ -234,5 +233,28 @@ FOREIGN KEY (ID_ANIMALE) REFERENCES ANIMALE (ID),
 FOREIGN KEY (NOME_STRUTTURA) REFERENCES STRUTTURA_VETERINARIA (NOME)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `area_tematica` (`NOME`, `last_update`) VALUES
+('Africa', '2019-05-29 09:36:44'),
+('Antartide', '2019-05-29 09:37:28'),
+('Artide', '2019-05-29 09:37:28'),
+('Asia', '2019-05-29 09:37:28'),
+('Europa', '2019-05-29 09:37:28'),
+('Nord America', '2019-05-29 09:37:28'),
+('Oceania', '2019-05-29 09:37:28'),
+('Sud America', '2019-05-29 09:37:28');
 
 
+INSERT INTO `alloggio` (`NUMERO`, `NOME`, `TIPO`, `NOME_AREA`, `last_update`) VALUES
+(1, 'Oasi dei Ghepardi', 'Recinzione in ferro', 'Africa', '2019-05-29 07:37:53'),
+(2, 'Lido degli Ippopotami', 'Recinto con siepi', 'Africa', '2019-05-29 13:03:35');
+
+INSERT INTO `animale` (`ID`, `NOME`, `SPECIE`, `HABITAT`, `PESO`, `ETA`, `SESSO`, `last_update`) VALUES
+('MC001', 'Gianluca', 'Ghepardo', 'Savana erbosa', 65, 4, 'M', '2019-05-29 07:34:55'),
+('MC002', 'Lucia', 'Ghepardo', 'Savana erbosa', 58, 3, 'F', '2019-05-29 07:35:40'),
+('ME001', 'Dumbo', 'Elefante indiano', 'Giungla', 3600, 4, 'M', '2019-05-29 12:57:06'),
+('ME003', 'Gianna', 'Ippopotamo', 'Foresta subtropicale secca', 1300, 6, 'F', '2019-05-29 13:00:55');
+
+
+INSERT INTO `risiede` (`ID_ANIMALE`, `NUMERO`, `last_update`) VALUES
+('MC001', 1, '2019-05-29 09:38:41'),
+('MC002', 1, '2019-05-29 09:38:55');
