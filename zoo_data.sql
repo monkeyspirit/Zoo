d0fd971c0805f0d4a540360335ae11349554c0df
Maria Saleri <mariasaleri@gmail.com>
2019-06-07 13:01:12 +0200

    Modifiche dati

--- zoo_data.sql
+++ zoo_data.sql
@@ -10,8 +10,8 @@ INSERT INTO `area_tematica` (`NOME`, `last_update`) VALUES
 INSERT INTO `alloggio` (`NUMERO`, `NOME`, `TIPO`, `NOME_AREA`, `last_update`) VALUES
 (1, 'Oasi dei Ghepardi', 'Recinzione in ferro', 'Africa', '2019-05-29 07:37:53'),
 (2, 'Lido degli Ippopotami', 'Recinto con siepi', 'Africa', '2019-05-29 13:03:35'),
-(1, 'Parco degli Elefanti', 'Recinto con canne di bamboo', 'Asia',  '2019-05-29 13:03:35'),
-(2, 'W i Panda', 'Aperto tanto non scappano', 'Asia',  '2019-05-29 13:03:35');
+(3, 'Parco degli Elefanti', 'Recinto con canne di bamboo', 'Asia',  '2019-05-29 13:03:35'),
+(4, 'W i Panda', 'Aperto tanto non scappano', 'Asia',  '2019-05-29 13:03:35');
 
 
 INSERT INTO `animale` (`ID`, `NOME`, `SPECIE`, `HABITAT`, `PESO`, `ETA`, `SESSO`, `last_update`) VALUES
@@ -19,8 +19,10 @@ INSERT INTO `animale` (`ID`, `NOME`, `SPECIE`, `HABITAT`, `PESO`, `ETA`, `SESSO`
 ('MCG002', 'Lucia', 'Ghepardo', 'Savana erbosa', 58, 3, 'F', '2019-05-29 07:35:40'),
 ('MEE001', 'Dumbo', 'Elefante indiano', 'Giungla', 3600, 4, 'M', '2019-05-29 12:57:06'),
 ('MEI002', 'Gianna', 'Ippopotamo', 'Foresta subtropicale secca', 1300, 6, 'F', '2019-05-29 13:00:55'),
-('MEP003', 'Botolo', 'Panda', null, 1300, 2, 'M', '2019-05-29 13:00:55');
+('MEP003', 'Botolo', 'Panda', 'Giungla' , 1300, 2, 'M', '2019-05-29 13:00:55'),
+('RCT001', 'Saetta', 'Testuggine', 'Macchia mediterranea', 2, 5,'M', '2019-05-29 13:00:55');
 
 
-
-
+INSERT INTO  `risiede` (`ID_ANIMALE`, `NUMERO`, `last_update`) VALUES
+('MCG001', '1', '2019-05-29 07:34:55'),
+('MCG002', '1', '2019-05-29 07:35:40');
